3ac0a55361011b6a32ab3b451b329ca3e72413c0
Kyle <kyle.q.miller@gmail.com>
2023-12-14 21:39:15 -0600

    created shaded boxes

--- p5.js
+++ p5.js
@@ -1,2 +1,14 @@
-const canvas = document.getElementById("canvas");
-const ctx = canvas.getContext("2d");
+function draw() {
+	const canvas = document.getElementById("canvas");
+	//check for support
+	if (canvas.getContext) {
+		const ctx = canvas.getContext("2d");
+		// drawing goes here
+		  ctx.fillStyle = "rgb(805, 0, 0)";
+          ctx.fillRect(10, 10, 50, 50);
+
+          ctx.fillStyle = "rgba(0, 150, 200, 0.5)";
+          ctx.fillRect(30, 30, 50, 50);
+	}
+		}	
+draw();
