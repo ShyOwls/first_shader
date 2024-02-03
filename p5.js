6a83569250601bcb15e1903b60a5d383f2bb904e
Kyle <kyle.q.miller@gmail.com>
2024-02-02 22:16:46 -0600

    update p5.js

--- p5.js
+++ p5.js
@@ -9,22 +9,49 @@ canvas.height=window.innerHeight;
 var c = canvas.getContext('2d');
 
 
-/* rectangle (x, y, width, height), color values in rgba*/
-c.fillStyle= 'rgba(255, 0, 0, 0.5)';
-c.fillRect(100, 500, 100, 100);
 
-c.fillStyle= 'rgba(255, 255, 0, 0.5)';
-c.fillRect(300, 300, 100, 100);
 
-c.fillStyle= 'rgba(255, 0, 255, 0.5)';
-c.fillRect(500, 100, 100, 100);
+for(var i = 0; i < 10; i++){
+	/* rectangle (x, y, width, height), color values in rgba*/
+	
+	
+	var x = Math.random() * window.innerWidth;
+	var y = Math.random() * window.innerHeight;
+	//using hsl to mix up colors was easir than rgba
+	c.fillStyle = 'hsl('+ 360 * Math.random() + ' , 75%, 50%)';
+	c.fillRect(x, y, 100, 100);
+}
 
 /*line*/
 c.beginPath();
-
-
 c.moveTo(50, 300);//x & y cord
 c.lineTo(300, 100);
 c.lineTo(400, 100);
-
+c.lineTo(100, 300);
+c.strokeStyle = "#fa34a3";
 c.stroke();
+
+//Arc & Circle 
+//X Y R(radius):int startAngle:float, EndAngle:float
+c.beginPath();
+c.arc(350, 350, 30, 0, Math.PI * 2, false);
+c.stroke();
+
+c.beginPath();
+c.arc(350, 350, 10, 0, Math.PI * 2, false);
+c.stroke();
+
+c.beginPath();
+c.arc(350, 350, 20, 0, Math.PI * 2, false);
+c.stroke();
+
+//create multiple shapes by using a for loop
+
+for(var i = 0; i < 30; i++) {
+	var x = Math.random() * window.innerWidth;
+	var y = Math.random() * window.innerHeight;
+	c.beginPath();
+	c.arc(x, y, 40, 0, Math.PI * 2, false);
+	c.stroke();
+}
+	
